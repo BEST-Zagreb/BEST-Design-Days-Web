cfdb62870813d38675432ba8cefa30cee3f188d8
JJ <jakovbla4@gmail.com>
2023-12-28 20:02:30 +0100

    Code refractoring

--- js/default.js
+++ js/default.js
@@ -1,35 +1,9 @@
-// ===== progress bar =====
-const progressBarElement = document.createElement("div");
-progressBarElement.classList.add("progress-bar", "hidden");
-document.querySelector("body").appendChild(progressBarElement);
-
 // ===== back to top button =====
-const backToTopBtn = document.createElement("div");
-backToTopBtn.classList.add("backToTopBtn", "hidden");
-backToTopBtn.setAttribute("title", "Scroll to top"); // tooltip
-backToTopBtn.innerHTML = `<svg
-aria-hidden="true"
-xmlns="http://www.w3.org/2000/svg"
-viewBox="0 0 448 512">
-    <path fill="black"
-    d="M240.971 130.524l194.343 194.343
-    c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667
-    c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516
-    c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667
-    c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525
-    c9.372-9.373 24.568-9.373 33.941-.001z">
-    </path>
-</svg>`;
-document.querySelector("body").appendChild(backToTopBtn);
+const backToTopBtn = document.querySelector(".backToTopBtn");
 
 backToTopBtn.addEventListener("click", () => {
   document.body.scrollIntoView();
 });
-
-// ===== background paralax =====
-const backgroundElement = document.createElement("div");
-backgroundElement.classList.add("background-image");
-document.querySelector("body").appendChild(backgroundElement);
 
 // ===== nav =====
 const nav = document.querySelector("nav");
@@ -130,6 +104,7 @@ document.addEventListener("scroll", () => {
     100;
 
   // progress bar
+  const progressBarElement = document.querySelector(".progress-bar");
   const progressBarShowOnPx = 10; // amount of pixels before progress bar is shown
 
   progressBarElement.style.width = scrolledPercentage + "%";
@@ -146,6 +121,7 @@ document.addEventListener("scroll", () => {
   else backToTopBtn.classList.add("hidden");
 
   // background paralax
+  const backgroundElement = document.querySelector(".background");
   backgroundElement.style.top = -scrolledPercentage / 4 + "%";
   backgroundElement.style.left = -scrolledPercentage / 2 + "%";
 
